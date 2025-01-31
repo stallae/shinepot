9d6272823de322af966d0b641ebb252518e0dc94
Ori <ori@stallae.com>
2025-01-31 16:32:15 -0300

    feat: install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+import {colors} from './src/assets/styles/colors.tsx';
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
+  presets: [require('nativewind/preset')],
+  theme: {
+    extend: {
+      colors: colors,
+    },
+  },
+  plugins: [],
+};
