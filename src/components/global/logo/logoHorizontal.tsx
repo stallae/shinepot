504c9164d78c8f3051166fbda201374f597a1e0c
Ori <ori@stallae.com>
2025-04-01 16:03:35 -0300

    fix: logo resize

--- src/components/global/logo/logoHorizontal.tsx
+++ src/components/global/logo/logoHorizontal.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import useTheme from '../../../hooks/useTheme.tsx';
 import {Image, Text, View} from 'react-native';
-import logoSmallDark from '../../../assets/images/logo/logoSmallDark.png';
-import logoSmallLight from '../../../assets/images/logo/logoSmallLight.png';
+import logoDark from '../../../assets/images/logo/logoDark.png';
+import logoLight from '../../../assets/images/logo/logoLight.png';
 import useColors from '../../../hooks/useColors.tsx';
 
 const LogoHorizontal = () => {
   const theme = useTheme();
   const {colors} = useColors();
-  const logoSource = theme === 'dark' ? logoSmallDark : logoSmallLight;
+  const logoSource = theme === 'dark' ? logoDark : logoLight;
   return (
     <View className="flex-row">
-      <Image source={logoSource} />
+      <Image
+        source={logoSource}
+        style={{height: 30, width: 30, objectFit: 'contain'}}
+      />
       <Text
         className="text-2xl mt-1 mx-1 ml-1"
         style={{
