adc9069bfff176658105698512534682aa5e42ba
Ori Kao <ori@stallae.com>
2025-12-20 17:47:43 -0300

    feat: add TextMessageContent component for displaying text message content in the message view

---
+++ src/components/main/view-message/content/TextMessageContent.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Text, ScrollView } from 'react-native';
+import useColors from '../../../../hooks/useColors';
+import { Messages } from '../../../../interfaces/messages';
+
+interface TextMessageContentProps {
+  message: Messages;
+}
+
+const TextMessageContent: React.FC<TextMessageContentProps> = ({ message }) => {
+  const { colors } = useColors();
+
+  const messageText = message.s3_url || 'Message content will be displayed here...';
+
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <Text
+        className="text-body-primary leading-6"
+        style={{ color: colors.textPrimary }}>
+        {messageText}
+      </Text>
+    </ScrollView>
+  );
+};
+
+export default TextMessageContent;
+
