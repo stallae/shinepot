4261e1094e1d2cc72c2b8e3e6bfc8e86edebfcf1
Ori Kao <ori@stallae.com>
2025-12-20 18:54:25 -0300

    feat: create index file for message view components, exporting MessageViewHeader, MessageContentView, MessageTimestamp, and PrivateMessageFooter

---
+++ src/components/main/view-message/index.ts
@@ -0,0 +1,5 @@
+export { default as MessageViewHeader } from './MessageViewHeader';
+export { default as MessageContentView } from './MessageContentView';
+export { default as MessageTimestamp } from './MessageTimestamp';
+export { default as PrivateMessageFooter } from './PrivateMessageFooter';
+
