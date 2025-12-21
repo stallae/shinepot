1225a07a5d3431e935c9a22c44c86009a1a89a71
Ori Kao <ori@stallae.com>
2025-12-21 20:24:47 -0300

    style: update NewMessageButton to use theme colors for icon and background

--- src/components/main/new-message-button/NewMessageButton.tsx
+++ src/components/main/new-message-button/NewMessageButton.tsx
@@ -12,9 +12,9 @@ const NewMessageButton: React.FC<ScreenProps> = ({ navigation }) => {
       onPress={() => {
         navigation.navigate(ROUTES.NewMessageFlow);
       }}
-      className="absolute bottom-6 right-6 w-16 h-16 rounded-3xl justify-center items-center"
-      style={{ backgroundColor: colors.textSecondary }}>
-      <PaperPlaneTilt size={24} weight="fill" color={colors.primary} />
+      className="absolute bottom-6 right-6 w-16 h-16 rounded-3xl justify-center items-center bg-blue-500"
+      >
+      <PaperPlaneTilt size={24} weight="fill" color={colors.textPrimary} />
     </Pressable>
   );
 };
