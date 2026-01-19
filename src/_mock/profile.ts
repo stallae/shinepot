c3f51997d25774db74501a0d0b63f5aa64bd8137
Ori Kao <ori@stallae.com>
2026-01-19 15:18:57 -0300

    feat: add user profile mock informations

---
+++ src/_mock/profile.ts
@@ -0,0 +1,11 @@
+export const PROFILE_DATA = {
+    id: 'user_12345',
+    name: 'Zé Mané',
+    avatarUrl: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80',
+    stats: {
+        sent: 105,
+        scheduled: 20,
+        received: 35,
+        saved: 10,
+    }
+};
