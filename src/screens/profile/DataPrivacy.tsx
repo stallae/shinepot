f1887490347c558044e052503e751cbe830074e3
Ori Kao <ori@stallae.com>
2026-01-19 15:37:07 -0300

    feat: add profile menu screen files

---
+++ src/screens/profile/DataPrivacy.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+import useColors from '../../hooks/useColors';
+import BackButton from '../../components/global/buttons/backButton';
+import { X } from 'phosphor-react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const DataPrivacy = () => {
+    const { colors } = useColors();
+    const navigation = useNavigation();
+
+    return (
+        <SafeAreaView className="flex-1" style={{ backgroundColor: colors.primary }}>
+            <View className="absolute top-20 left-2 z-10">
+                <BackButton icon={<X />} onPress={navigation.goBack} />
+            </View>
+            <View className="flex-1 justify-center items-center">
+                <Text style={{ color: colors.textPrimary }}>Data Privacy</Text>
+            </View>
+        </SafeAreaView>
+    );
+};
+
+export default DataPrivacy;
