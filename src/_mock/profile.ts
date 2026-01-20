49e67a19a129e3346e7128b547b6308edca85984
Ori Kao <ori@stallae.com>
2026-01-20 11:31:24 -0300

    feat: enhance profile mock data structure and introduce address details interface

--- src/_mock/profile.ts
+++ src/_mock/profile.ts
@@ -1,11 +1,26 @@
-export const PROFILE_DATA = {
+import { ProfileData } from '../interfaces/profile';
+
+export const PROFILE_DATA: ProfileData = {
     id: 'user_12345',
-    name: 'Zé Mané',
+    firstName: 'André',
+    lastName: 'Oliveira',
+    email: 'andre.oliveira@gmail.com',
+    phone: '+244940891243',
+    birthday: '1990-01-01',
     avatarUrl: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80',
     stats: {
         sent: 105,
         scheduled: 20,
         received: 35,
         saved: 10,
-    }
+    },
+    addressDetails: {
+        country: 'BR',
+        state: 'MG',
+        city: 'pouso-alegre',
+        street: 'Rua Heitor Carline',
+        number: '24',
+        complement: '',
+        zipCode: '00000-000',
+    },
 };
--- src/interfaces/profile.ts
+++ src/interfaces/profile.ts
@@ -2,15 +2,41 @@ import { Icon } from 'phosphor-react-native';
 import { RootStackParamList } from '../navigation/roots';
 
 export interface ProfileMenuItem {
-    title: string;
-    icon: Icon;
-    route: keyof RootStackParamList;
-    highlight?: boolean;
+  title: string;
+  icon: Icon;
+  route: keyof RootStackParamList;
+  highlight?: boolean;
 }
 
-
 export interface ProfileStats {
-    title: string;
-    value: string;
-    icon: Icon;
+  title: string;
+  value: string;
+  icon: Icon;
+}
+
+export interface AddressDetails {
+  country: string;
+  state: string;
+  city: string;
+  street: string;
+  number: string;
+  complement: string;
+  zipCode: string;
+}
+
+export interface ProfileData {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  birthday: string; 
+  avatarUrl: string;
+  stats: {
+    sent: number;
+    scheduled: number;
+    received: number;
+    saved: number;
+  };
+  addressDetails: AddressDetails;
 }
